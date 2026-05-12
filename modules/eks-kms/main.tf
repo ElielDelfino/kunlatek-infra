2014b6381af15ef4e9d7c425302449bd96010fb3
Eliel Delfino <eliel.delfino@fanut.ufal.br>
2026-05-12 18:28:39 -0300

    ajuste iam role

--- modules/eks-kms/main.tf
+++ modules/eks-kms/main.tf
@@ -1,7 +1,36 @@
+data "aws_caller_identity" "current" {}
+
 resource "aws_kms_key" "eks" {
   description             = "KMS key for EKS cluster ${var.cluster_name} secrets encryption"
   enable_key_rotation     = true
   deletion_window_in_days = 7
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "Enable IAM User Permissions"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+        Action   = "kms:*"
+        Resource = "*"
+      },
+      {
+        Sid    = "Allow EKS to use the key and create grants"
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+        Action = [
+          "kms:DescribeKey",
+          "kms:CreateGrant"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
 
   tags = { Name = "${var.cluster_name}-eks-key" }
 }
